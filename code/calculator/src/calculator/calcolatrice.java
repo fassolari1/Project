8cac656af3ded4e99c7da93d52b08b2866072724
Fabio <febpalazzi@gmail.com>
2021-10-22 12:15:25 +0200

    Update Calcolatrice.java

--- code/calculator/src/calculator/calcolatrice.java
+++ code/calculator/src/calculator/calcolatrice.java
@@ -5,5 +5,25 @@ public class calcolatrice {
 	int a = 1;
 	int b = 2;
 	
-	
+	public double somma(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public double sottrazione(double num1, double num2){
+        return num1 - num2;
+    }
+
+    public double moltiplicazione(double num1, double num2){
+        return num1 * num2;
+    }
+
+    public double divisione(double num1, double num2){
+        double result=0;
+        try {
+            result = num1/num2;
+        } catch (Exception e) {
+            Console.out.println("Error");
+        }
+        return result;
+    }
 }
